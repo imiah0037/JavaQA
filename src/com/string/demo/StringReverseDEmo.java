500b160617011b862b17493728ab32a90e728b19
ibrahimmiah <ibrahimmiah787@gmail.com>
2023-02-12 20:22:21 -0500

    Stringreverse and String buffer more code added

--- src/com/string/demo/StringReverseDEmo.java
+++ src/com/string/demo/StringReverseDEmo.java
@@ -3,8 +3,26 @@ package com.string.demo;
 public class StringReverseDEmo {
 
 	public static void main(String[] args) {
-String name= "Ibrahim";
-System.out.println(name);
+		/**
+		 * @author Ibrahim Miah
+		 * How to reverse a String
+		 */
+		String s = "Selenium";
+		int len = s.length();
+		System.out.println("Lenghth of s: " + len);
+		// using for loop
+		String rev = "";
+		System.out.println("++++++++++++++++++Using ForLoop++++++++++++++++++++");
+		for (int i = len - 1; i >= 0; i--) {
+			rev = rev + s.charAt(i);
+		}
+
+		System.out.println("String reverse using for loop: " + rev);
+		
+		System.out.println("++++++++++++++++++Using StringBuffer++++++++++++++++++++");
+		StringBuffer sb = new StringBuffer(s);
+		System.out.println("String reverse using String buffer: "+sb.reverse());
+
 		
 	}
 
